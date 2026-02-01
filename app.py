be3b0e33775a005894321ae0674816add229fa86
bailewen <49675620+bailewen@users.noreply.github.com>
2026-02-01 08:12:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ if run_button:
             progress_bar.progress(completed / len(all_symbols))
             
             try:
-                rec = future.result(timeout=15)
+                rec = future.result(timeout=10)
                 if rec and rec.get("marketCap", 0) >= min_mcap:
                     records.append(rec)
             except Exception as e:
